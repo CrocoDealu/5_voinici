64c98fe5c2f8fdd020d801856736d3f0b86bb5bb
MusatStefanRadu <musatstefan2004@gmail.com>
2025-11-08 21:07:54 +0200

    Added theoretical pages and unified navbar

--- voinici/urls.py
+++ voinici/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import home  
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', home, name='home'),
+    path('theory/inclined_plane/', views.inclined_plane_view, name='inclined_plane'),
+    path('theory/collision/', views.collision_view, name='collision'),
+    path('theory/pendulum/', views.pendulum_view, name='pendulum'),
+
 ]
